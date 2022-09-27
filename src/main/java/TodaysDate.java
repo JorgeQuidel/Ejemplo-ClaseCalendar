a3d0b8b1534898225b91ac1277f46823f384a3de
Jorge <jorge.quidel312@gmail.com>
2022-09-27 09:49:55 -0300

    Initial commit

---
+++ src/main/java/TodaysDate.java
@@ -0,0 +1,24 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static java.lang.System.out;
+
+public class TodaysDate {
+    String time;
+    public int day;
+    private int month;
+    protected int year;
+
+    public void printDateAndTime(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        time = calendar.get(Calendar.HOUR_OF_DAY) + ":"
+                + calendar.get(Calendar.MINUTE) + ":"
+                + calendar.get(Calendar.SECOND);
+        day = calendar.get(Calendar.DATE);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+
+        out.println("Time: " + time);
+        out.println("Date: " + month + " " + day + " " + year);
+    }
+}
